8a2f71325a01696b6bdccb68c469dae2efb6180c
Valentin <valentin.guevara@live.com>
2016-04-29 11:07:44 +0200

    suppr logiciel

--- logiciel/Visual Paradigm for UML CE 10.1/scripts/Logizian.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ../bin
-JAVA="../jre/bin/java"
-if [ ! -f "$JAVA" ]; then
-    JAVA="java"
-fi
-
-"$JAVA" -Xms256m -Xmx768m -XX:MaxPermSize=256m -cp ".:../lib/vpplatform.jar:../lib/bpva-help.jar:../lib/jniwrap.jar:../lib/winpack.jar:../ormlib/orm.jar:../ormlib/orm-core.jar:../lib/xalan.jar:../lib/lib01.jar:../lib/lib02.jar:../lib/lib03.jar:../lib/lib04.jar:../lib/lib05.jar:../lib/lib06.jar:../lib/lib07.jar:../lib/lib08.jar:../lib/lib09.jar:../lib/lib10.jar" RV "$@"
