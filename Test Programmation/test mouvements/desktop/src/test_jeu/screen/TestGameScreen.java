d90071614d1f693eee2b9046791d1d66bb09b826
Jérémy Duval <jeremy-duval1@hotmail.fr>
2016-09-24 20:39:07 +0200

    Début de test à partir de libGDX afin de créer les mouvements du personnage : - création du projet - création de classe à partir de la 1ère vidéo tutoriel - création de la javadoc

---
+++ Test Programmation/test mouvements/desktop/src/test_jeu/screen/TestGameScreen.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_jeu.screen;
+
+import com.badlogic.gdx.Screen;
+import test_jeu.Test_Jeu;
+
+/**
+ * Classe servant à definir notre ecran.
+ * Implemente la classe Screen de LibGDX
+ * @since  1.0
+ * @author jeremy
+ */
+public class TestGameScreen implements Screen{
+    
+    /**
+     * Constructeur de l'ecran.
+     * @param application : Test_Jeu 
+     * @since  1.0
+     * @author jeremy
+     */
+    public TestGameScreen(Test_Jeu application) {
+
+    }
+    
+    /**
+     * methode override servant à afficher l'ecran
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void show() {
+
+    }
+    
+    /**
+     * methode override servant au rendu de l'ecran
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void render(float delta) {
+
+    }
+    
+    /**
+     * methode override servant au redimentionnement l'ecran
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void resize(int width, int height) {
+
+    }
+    
+    /**
+     * methode override servant à mettre en pause l'ecran
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void pause() {
+
+    }
+    
+    /**
+     * methode override servant à mettre fin à une pause
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void resume() {
+
+    }
+    
+    /**
+     * methode override servant à cacher l'ecran
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void hide() {
+
+    }
+    
+    /**
+     * methode override servant à quitter l'ecran
+     * @since  1.0
+     * @author jeremy
+     */
+    @Override
+    public void dispose() {
+
+    }
+    
+}
---
+++ Test Programmation/test mouvements/desktop/src/test_jeu/screen/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * contient les classes de test du jeu
+ * @author jeremy
+ * @since 1.0
+ */
+package test_jeu.screen;
