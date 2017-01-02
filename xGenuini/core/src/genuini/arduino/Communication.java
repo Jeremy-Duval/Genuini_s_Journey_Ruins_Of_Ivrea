5ad2e9123a72d87a530cef260d9bc2fc0db2b8cb
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2017-01-02 15:02:02 +0100

    Nouvel essaie de communication : - detection du problème : le port com est déjà utilisé, Java ne peut pas l'utilisé aussi - refactorisation du code : imbrication des try/catch : plus propre, évite les crash

--- xGenuini/core/src/genuini/arduino/Communication.java
+++ xGenuini/core/src/genuini/arduino/Communication.java
@@ -9,11 +9,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import gnu.io.CommPortIdentifier; 
+import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent; 
 import gnu.io.SerialPortEventListener;
 import gnu.io.UnsupportedCommOperationException;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,6 +29,7 @@ public class Communication {
     /**************************************************************************/
     private SerialPort serialPort;
     private static CommPortIdentifier portId;
+    private static Enumeration portList;
     /* A BufferedReader which will be fed by a InputStreamReader converting the
      * bytes into characters making the displayed results codepage independent*/
     private BufferedReader input;
@@ -62,33 +65,57 @@ public class Communication {
     }
     /**
      * Write in the Arduino.
+     *
      * @since 22/12/2016
      * @author jeremy
      */
     public void arduinoWrite(String message) {
         String test;
-        try {
-            outputStream = serialPort.getOutputStream();
-        } catch (IOException e) {
-            System.out.println("err1");
-        }
-        try {
-            serialPort.setSerialPortParams(9600,
-                    SerialPort.DATABITS_8,
-                    SerialPort.STOPBITS_1,
-                    SerialPort.PARITY_NONE);
-        } catch (UnsupportedCommOperationException e) {
-            System.out.println("err2");
-        }
-        try {
-            outputStream.write(message.getBytes());
-            System.out.println(message);
+        portList = CommPortIdentifier.getPortIdentifiers();
 
-            outputStream.close();
-            serialPort.close();
+        while (portList.hasMoreElements()) {
+            portId = (CommPortIdentifier) portList.nextElement();
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                if (portId.getName().equals("COM3")) {//NEED TO MODIFY LATER
+                    try {
+                        serialPort = (SerialPort) portId.open("SimpleWriteApp", 2000);
+                        try {
+                            outputStream = serialPort.getOutputStream();
+                            try {
+                                serialPort.setSerialPortParams(9600,
+                                        SerialPort.DATABITS_8,
+                                        SerialPort.STOPBITS_1,
+                                        SerialPort.PARITY_NONE);
+                                try {
+                                    outputStream.write(message.getBytes());
+                                    System.out.println(message);
 
-        } catch (IOException e) {
-            System.out.println("err3");
+                                    outputStream.close();
+                                    serialPort.close();
+
+                                } catch (IOException e) {
+                                    System.out.println("*********************************");
+                                    System.out.println("err3");
+                                    System.out.println("*********************************");
+                                }
+                            } catch (UnsupportedCommOperationException e) {
+                                System.out.println("*********************************");
+                                System.out.println("err2");
+                                System.out.println("*********************************");
+                            }
+                        } catch (IOException e) {
+                            System.out.println("*********************************");
+                            System.out.println("err1");
+                            System.out.println("*********************************");
+                        }
+                    } catch (PortInUseException e) {
+                        System.out.println("*********************************");
+                        System.out.println("err");
+                        System.out.println("*********************************");
+                    }
+
+                }
+            }
         }
     }
 }
