4a105a1e8dcc91d2eac78d0699b6f2dd1597e1a0
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2017-03-11 18:59:43 +0100

    button fact

--- xGenuini/core/src/genuini/game/SkinManager.java
+++ xGenuini/core/src/genuini/game/SkinManager.java
@@ -43,25 +43,13 @@ public class SkinManager {
         skin = new Skin();
         skin.add("default", fontBlack);
     }
-
-    public Skin createBookButtonSkin(int width, int height) {
-        //Create a font
-
-        skin.add("default", fontWhite);
-
-        skin.add("textureBookButton", textureBookButton);
-
-        //Create a button style
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("textureBookButton", Color.GRAY);
-        textButtonStyle.down = skin.newDrawable("textureBookButton", Color.DARK_GRAY);
-        textButtonStyle.checked = skin.newDrawable("textureBookButton", Color.DARK_GRAY);
-        textButtonStyle.over = skin.newDrawable("textureBookButton", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);
-        return skin;
-    }
-
+    
+    /**
+     * Create a white button by default
+     * @param width : text's width
+     * @param height : text's height
+     * @return 
+     */
     public Skin createButtonSkin(int width, int height) {
         //text's creation 
         this.textSkin(Color.WHITE, false, width, height);
@@ -76,6 +64,78 @@ public class SkinManager {
         skin.add("default", textButtonStyle);
         return skin;
     }
+    
+    /**
+     * Create a button with parameters and the dark backgroud by default 
+     * @param width : text's width
+     * @param height : text's height
+     * @param color : text's color
+     * @param dark : 1 if the color is dark ; 0 if it's light
+     * @return 
+     */
+    public Skin createButtonSkin(int width, int height, Color color, boolean dark) {
+        //text's creation 
+        this.textSkin(color, dark, width, height);
+
+        //Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", Color.WHITE);
+        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+        return skin;
+    }
+    
+    /**
+     * Create a button with parameters 
+     * @param width : text's width
+     * @param height : text's height
+     * @param text_color : text's color
+     * @param dark : 1 if the color is dark ; 0 if it's light
+     * @param up_color : button's color when it is up
+     * @param down_color : button's color when it is down
+     * @param checked_color : button's color when it is checked
+     * @param over_color : button's color when it is over
+     * @return 
+     */
+    public Skin createButtonSkin(int width, int height, Color text_color, boolean dark,
+            Color up_color, Color down_color, Color checked_color, Color over_color) {
+        //text's creation 
+        this.textSkin(text_color, dark, width, height);
+
+        //Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", up_color);
+        textButtonStyle.down = skin.newDrawable("background", down_color);
+        textButtonStyle.checked = skin.newDrawable("background", checked_color);
+        textButtonStyle.over = skin.newDrawable("background", over_color);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+        return skin;
+    }
+    
+    /**
+     * Create a text with parameters 
+     * @param text_color : text's color
+     * @param width : text's width
+     * @param height : text's height 
+     * @param dark : 1 if the color is dark ; 0 if it's light
+     * @param background_color : the button's backgroud color
+     * @param focusedBackground_color : the button's focused backgroud color
+     * @param cursor_color : the button's cursor color
+     * @param cursor_width : : the button's cursor width color
+     * @return 
+     */
+     /*public Skin ButtonSkin(Color text_color, boolean dark, float width, float height, 
+             Color background_color, Color focusedBackground_color, Color cursor_color, float cursor_width){
+         
+     //text's creation 
+        this.textSkin(text_color, dark, width, height);
+        
+     return skin;
+     }*/
 
     /**
      * Create a skin whith the parameter color
@@ -101,30 +161,10 @@ public class SkinManager {
         return skin;
     }
 
-    /**
-     * Create a skin whith more parameter
-     *
-     * @param color : text's color
-     * @param dark : 1 if the color is dark ; 0 if it's light
-     * @param width : text's width
-     * @param height : text's height
-     * @return skin : a Skin type
-     */
-    /*
-     public Skin textSkin(Color color, boolean dark, float width, float height){
-     if(dark){
-     skin.add("default", fontBlack);
-     } else {
-     skin.add("default", fontWhite);
-     }
-        
-     Pixmap pixmap = new Pixmap((int) width, (int) height, Pixmap.Format.RGB888);
-     pixmap.setColor(Color.WHITE);
-     pixmap.fill();
-     skin.add("background", new Texture(pixmap));
-        
-     return skin;
-     }*/
+    
+     
+     
+     
     public Skin whiteTextSkin(float width, float height) {
         //Create a font
         skin.add("default", fontWhite);
